4399bbba059c16e161326b13ecb85513d2dce800
김나경 <recodepan1219@egmail.com>
2023-07-12 00:18:42 +0900

    메인 및 네비 생성

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
+<!-- app.vue가 모든 페이지의 진입점이 되는 컴포넌트 역할을 함
+전역적 .js 파일이나 css 파일을 적용하고 싶으면 app.vue 적용-->
+
 <template>
 	<div>
 		<NuxtLayout>
-			<NuxtPage />
+			<NuxtPage /> <!--pages/ 폴더를 가져와서 렌더링한다는 뜻-->
 		</NuxtLayout>
 	</div>
 </template>
---
+++ components/NavigationBar/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/login">Login</NuxtLink>
+  </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
